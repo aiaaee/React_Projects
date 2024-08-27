8d35994259fb0ac3d22243d49aa967c9c381b69a
Amin Alaee <aminalaee4@gmail.com>
2024-08-27 20:12:15 +0430

    add new file

---
+++ TodoList/reactapp/src/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='border rounded p-2 pb-4  border-1 bg-primary text-white text-center'>
+        <div>
+            <h2>
+                TodoApp
+            </h2>
+            <small className='p-2'>
+                you haven't need to use any application to input your daily todos 
+            </small>
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
